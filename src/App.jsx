3f218720bf5c495c272c04578871ac7cf8755b24
Goshak95 <goshak2005@yandex.ru>
2020-03-30 20:54:33 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+import VendingContainer from "./containers/VendingContainer";
+
+class App extends React.Component {
+  render() {
+    return <VendingContainer />;
+  }
+}
+
+export default App;
---
+++ src/containers/VendingContainer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.scss";
+import { connect } from "react-redux";
+
+class VendingContainer extends React.Component {
+  render() {
+    return <div>Machine!</div>;
+  }
+}
+
+export default VendingContainer;
