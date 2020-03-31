999889229a8f72e4227193b917c67e4debdd8776
Goshak95 <goshak2005@yandex.ru>
2020-03-31 22:41:41 +0300

    add machine action creators

---
+++ src/actions/Machine/Machine.js
@@ -0,0 +1,17 @@
+import * as t from "./types";
+import { API_ROOT } from "../../constants/defaults";
+
+export function getProducts() {
+  return dispatch => {
+    dispatch({
+      type: t.GET_PRODUCTS_REQUEST
+    });
+
+    return fetch(`${API_ROOT}/products`)
+      .then(response => response.json())
+      .then(data => dispatch({ type: t.GET_PRODUCTS_SUCCESS, payload: data }))
+      .catch(error =>
+        dispatch({ type: t.GET_PRODUCTS_FAILURE, payload: error.message })
+      );
+  };
+}
---
+++ src/constants/defaults.js
@@ -0,0 +1,1 @@
+export const API_ROOT = "https://5e83776b78337f00160aeeb1.mockapi.io/api/v1/";
